7fc3857c992a781bf96c5c2741b3df9cc7228172
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-29 22:40:55 -0400

    add support for markdown printer tests

--- test/printer/index.ts
+++ test/printer/index.ts
@@ -1,16 +1,18 @@
 import test from 'ava';
 import { readdirSync, readFileSync, existsSync } from 'fs';
+import { extname } from 'path';
 import { format } from 'prettier';
 
-const files = readdirSync('test/printer/samples').filter(name => name.endsWith('.html'));
+const files = readdirSync('test/printer/samples').filter(name => name.endsWith('.html') || name.endsWith('.md'));
 
 for (const file of files) {
+    const ext = extname(file);
     const input = readFileSync(`test/printer/samples/${file}`, 'utf-8').replace(/\r?\n/g, '\n');
-    const options = readOptions(`test/printer/samples/${file.replace('.html', '.options.json')}`);
+    const options = readOptions(`test/printer/samples/${file.replace(ext, '.options.json')}`);
 
-    test(`printer: ${file.slice(0, file.length - '.html'.length)}`, t => {
+    test(`printer: ${file.slice(0, file.length - ext.length)}`, t => {
         const actualOutput = format(input, {
-            parser: 'svelte' as any,
+            parser: ext === '.md' ? 'markdown' : 'svelte' as any,
             plugins: [require.resolve('../../src')],
             tabWidth: 4,
             ...options,
